f0f22bd1afeb5cae76c2025a75a6d2c337d4afe1
marufa <gmalfacorrea@gmail.com>
2025-08-27 20:38:58 -0300

    Primeiro commit

---
+++ src/main/java/com/malfaa/pmdp/PlataformaDeMentoriaEDesenvolvimentoProfissionalApplication.java
@@ -0,0 +1,13 @@
+package com.malfaa.pmdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeMentoriaEDesenvolvimentoProfissionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeMentoriaEDesenvolvimentoProfissionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malfaa/pmdp/PlataformaDeMentoriaEDesenvolvimentoProfissionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malfaa.pmdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeMentoriaEDesenvolvimentoProfissionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
