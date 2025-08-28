2178224032dacdd65f08e78d285b8de320f5f0ae
Gustavo M.C <56200362+Malfaa@users.noreply.github.com>
2025-08-28 13:15:41 +0000

    Adição de um exclude datasource apenas para visualização do código, depois será removido

--- src/main/java/com/malfaa/pmdp/PlataformaMentoriaApplication.java
+++ src/main/java/com/malfaa/pmdp/PlataformaMentoriaApplication.java
@@ -2,8 +2,9 @@ package com.malfaa.pmdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PlataformaMentoriaApplication {
 
 	public static void main(String[] args) {
