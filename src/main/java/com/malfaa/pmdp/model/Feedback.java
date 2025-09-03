1e5dbfc30ece459af2dfdbf7787e21982147a2f2
Gustavo M.C <56200362+Malfaa@users.noreply.github.com>
2025-09-03 15:09:35 +0000

    Adição das classes modelos restantes

--- src/main/java/com/malfaa/pmdp/model/Feedback.java
+++ src/main/java/com/malfaa/pmdp/model/Feedback.java
@@ -1,7 +1,29 @@
 package com.malfaa.pmdp.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Setter;
+import lombok.Getter;
 
 @Entity
+@Getter
+@Setter
 public class Feedback {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "comentario", columnDefinition = "TEXT")
+    private String comentario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mentor_id", nullable = false)
+    private Mentor mentor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mentorado_id", nullable = false)
+    private Mentorado mentorado;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sessao_id", unique = true)
+    private Sessao sessao;
 }
--- src/main/java/com/malfaa/pmdp/model/MaterialApoio.java
+++ src/main/java/com/malfaa/pmdp/model/MaterialApoio.java
@@ -1,7 +1,28 @@
 package com.malfaa.pmdp.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class MaterialApoio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "descricao", columnDefinition = "TEXT", nullable = false)
+    private String descricao;
+
+    @Column(name="link_arquivo", nullable = false)
+    private String linkArquivo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mentor_id", nullable = false)
+    private Mentor mentor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sessao_id")
+    private Sessao sessao;
 }
